aec6be1def5bad21fc2a5c368e303cdf160fca0d
Patrick Aikens <paikens@gmail.com>
2013-03-18 18:32:10 -0400

    Load the config from system/user files

--- lib/astrid/rest.rb
+++ lib/astrid/rest.rb
@@ -5,15 +5,20 @@ require "astrid/rest/version"
 
 module Astrid
   module Rest
+    GLOBAL_CONFIG = File.new('/etc/astridrc')
+    CONFIG_FILE = File.expand_path('~/.astridrc')
+
     class << self
       attr_accessor :user_hash
+      attr_accessor :config
     end
 
     def self.sign_in
     end
 
     def self.read_config
-      @@config = YAML.load_file(File.expand_path('~/.astridrc'))
+      config = YAML.load_file(GLOBAL_CONFIG) if File.exist?(GLOBAL_CONFIG)
+      config.merge!(YAML.load_file(CONFIG_FILE)) if File.exist?(CONFIG_FILE)
     end
   end
 end
--- spec/astrid-rest_spec.rb
+++ spec/astrid-rest_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe Astrid::Rest do
+  describe "config loading" do
+    it should "load the config file" do
+    end
+  end
+
   describe "request signatures" do
+    it should "generate the proper signature" do
+    end
   end
 end
